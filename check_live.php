9a4a4833bcdae43916ccd3c24e39f40e6279d64e
arizustudio <arizu.team@gmail.com>
2026-02-27 22:34:00 +0700

    api ke arizuproject

--- check_live.php
+++ check_live.php
@@ -1,5 +1,13 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 function checkLive($channelUrl) {
 
     $ch = curl_init();
