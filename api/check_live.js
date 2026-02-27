14193cf6474eacc705acfd5b4474fb743bd88fd7
arizustudio <arizu.team@gmail.com>
2026-02-27 22:21:18 +0700

    dasdsa

--- api/check_live.js
+++ api/check_live.js
@@ -14,14 +14,16 @@ export async function GET(request) {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36",
                 "Accept-Language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"
             }
-        });;
+        });
 
         const html = await response.text();
 
+        // Kalau ada scheduledStartTime → dianggap offline
         if (html.includes('"scheduledStartTime"')) {
             return new Response(JSON.stringify({ live: false }), { status: 200 });
         }
 
+        // Kalau live
         if (html.includes('"isLive":true')) {
 
             const match = html.match(/"videoId":"(.*?)"/);
@@ -30,7 +32,7 @@ export async function GET(request) {
                 return new Response(
                     JSON.stringify({
                         live: true,
-                        embed: `https://www.youtube.com/embed/${match[1]}`,
+                        embed: `https://www.youtube.com/embed/${match[1]}?hl=id`,
                         channel: url
                     }),
                     { status: 200 }
